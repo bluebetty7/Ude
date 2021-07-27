350b01a167c9a25ac752ecb8fcc1e6e919c3dd6c
bluebetty7 <izabela.stamatova@gmail.com>
2021-07-27 14:38:50 -0500

    Initial Commit

---
+++ Ude/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ude
+//
+//  Created by Izabela Stamatova on 7/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
